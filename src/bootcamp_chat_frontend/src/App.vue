9524f2cbe6a67bf13dd962880d1dd3b0f42ea4ec
JanBukalskiecho <janbukalski2012@gmail.com>
2024-07-30 19:03:10 +0200

    App completed

--- src/bootcamp_chat_frontend/src/App.vue
+++ src/bootcamp_chat_frontend/src/App.vue
@@ -1,25 +1,48 @@
 <script lang="ts">
 import { ref } from 'vue';
-import { bootcamp_chat_backend } from '../../declarations/bootcamp_chat_backend';
+import { bootcamp_chat_backend, canisterId, createActor } from '../../declarations/bootcamp_chat_backend';
 import { AuthClient } from '@dfinity/auth-client';
 import { HttpAgent } from '@dfinity/agent';
 import type { Identity } from '@dfinity/agent';
+import { Principal } from '@dfinity/principal';
 
 export default {
   data() {
     return {
-      newNote: "",
-      notes: [] as string[],
+      newChat: "",
+      chats: [] as string[][],
       identity: undefined as undefined | Identity,
+      principalText: "",
+      targetPrincipal: "",
     }
   },
   methods: {
-    async dodajNotatke() {
-      await bootcamp_chat_backend.add_note(this.newNote)
-      await this.pobierzNotatki()
+    async dodajChatMSG() {
+      if (!this.identity || this.identity.getPrincipal() === Principal.anonymous()) {
+        throw new Error("PLZ log in")
+      }
+      const targetPrincipal = Principal.fromText(this.targetPrincipal)
+      if (!targetPrincipal || targetPrincipal === Principal.anonymous()){
+        throw new Error("wrogn target")
+      }
+
+      const backend = createActor(canisterId, {
+        agentOptions: {
+          identity: this.identity
+        }
+      });
+      await backend.add_chat_msg(this.newChat, targetPrincipal)
+      await this.pobierzChaty()
     },
-    async pobierzNotatki() {
-      this.notes = await bootcamp_chat_backend.get_notes()
+    async pobierzChaty() {
+      if (!this.identity || this.identity.getPrincipal() === Principal.anonymous()) {
+        throw new Error("PLZ log in")
+      }
+      const targetPrincipal = Principal.fromText(this.targetPrincipal)
+      if (!targetPrincipal || targetPrincipal === Principal.anonymous()){
+        throw new Error("wrogn target")
+      }
+      this.chats = await bootcamp_chat_backend.get_chat(this.identity.getPrincipal(), targetPrincipal)
     },
     async login() {
       const authClient = await AuthClient.create();
@@ -28,15 +51,12 @@ export default {
       })
 
       const identity = authClient.getIdentity();
-      console.log("Zalogowano", identity.getPrincipal())
+      this.principalText = identity.getPrincipal().toText()
+      console.log("Zalogowano", this.principalText)
       this.identity = identity;
-      
-     //const agent = new HttpAgent({ identity });
+      await this.pobierzChaty()
     }
   },
-  mounted(){
-    this.pobierzNotatki()
-  }
 }
 </script>
 
@@ -45,12 +65,17 @@ export default {
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
-    {{ identity?.getPrincipal() }} <button @click="login">Login</button>
+    {{ principalText }} <button @click="login">login</button>
     <div>
-      {{ notes }}
+      <input v-model="targetPrincipal" /><button @click="pobierzChaty">pobierz chat</button>
     </div>
     <div>
-      <textarea v-model="newNote"></textarea><button @click="dodajNotatke">Dodaj notatke</button>
+      <div v-for="chat in chats[0]">
+        {{ chat }}
+      </div>
+    </div>
+    <div>
+      <textarea v-model="newChat"></textarea><button @click="dodajChatMSG">Dodaj notatke</button>
     </div>
   </main>
 </template>
